228649da9bb87399c12d31765d657a017652f477
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2013-10-26 17:25:41 +0100

    implemented the diary view

--- MySelf.WebClient/app/js/myselflog.main.js
+++ MySelf.WebClient/app/js/myselflog.main.js
@@ -3,7 +3,7 @@
 myselflogApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.
         when('/diary/:link', { templateUrl: '/app/views/diary.view.html', controller: 'NewFriendController' }).
-        when('/profile/:id', { templateUrl: '/app/views/log.view.html', controller: 'ProfileController' }).
+        when('/profile/:id', { templateUrl: '/app/views/profile.view.html', controller: 'ProfileController' }).
         when('/', { templateUrl: 'app/views/profiles.view.html', controller: 'ProfilesController' }).
         otherwise({ redirectTo: '/' });
     $locationProvider.html5Mode(true);
