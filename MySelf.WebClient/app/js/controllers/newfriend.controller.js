62527baa96425aa527ef9c74b83a82cf45c9d954
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2013-12-15 16:29:05 +0000

    implementing mixing terapies and values into the same graph; refactored refreshGraph method

--- MySelf.WebClient/app/js/controllers/newfriend.controller.js
+++ MySelf.WebClient/app/js/controllers/newfriend.controller.js
@@ -111,6 +111,30 @@ function ($scope, friendDatacontext, logger, $filter, moment, $routeParams, $rou
         $scope.link = $routeParams.link;
         getData();
     }
+    
+    function refreshGraph() {
+        if ($scope.profile) {
+            var logs = getLogs($scope.selectedreport.name);
+            var terapies = getTerapies($scope.selectedreport.name);
+            
+            if ($scope.showTerapies) {
+                $scope.graph = Morris.Line({
+                    element: 'diaryGraph',
+                    xkey: 'logdate',
+                    ykeys: ['value', 'slow', 'fast'],
+                    labels: ['Diary', 'Slow terapy', 'Fast terapy']
+                });
+                // TODO
+            }
+            
+            $scope.selectedreport.description = getReportDescription($scope.selectedreport.name);
+            $scope.graph.setData(logs);
+            $scope.terapiesGraph.setData(terapies);
+            $scope.logs = logs;
+            $scope.terapies = terapies;
+            $scope.selectedreport.average = getAverage();
+        }
+    }
 
     function getWeekDays(date) {
         var sunday = moment(date).day("Sunday");
@@ -124,125 +148,196 @@ function ($scope, friendDatacontext, logger, $filter, moment, $routeParams, $rou
         return weekdates;
     }
     
-    function refreshGraph() {
-        if ($scope.profile) {
-            var logs = [];
-            var fastvalues = [];
-            var slowvalues = [];
-            var terapies = [];
-            var d2 = new Date($scope.date);
-            if ($scope.selectedreport.name == "Year") {
-                angular.forEach($scope.profile.logs, function (item) {
-                    var d = new Date(item.logdate);
-                    if (d.getYear() == d2.getYear()) {
-                        logs.push(item);
-                    }
-                });
-                angular.forEach($scope.profile.terapies, function (item) {
-                    var d = new Date(item.logdate);
-                    if (d.getYear() == d2.getYear()) {
-                        if (item.isslow) {
-                            slowvalues.push(item);
-                        } else {
-                            fastvalues.push(item);
-                        }
-                    }
-                });
-                $scope.selectedreport.description = moment($scope.date).format('YYYY');
+    function getLogsByYear() {
+        var logs = [];
+        var d2 = new Date($scope.date);
+        angular.forEach($scope.profile.logs, function (item) {
+            var d = new Date(item.logdate);
+            if (d.getYear() == d2.getYear()) {
+                logs.push(item);
             }
-            if ($scope.selectedreport.name == "Month") {
-                angular.forEach($scope.profile.logs, function (item) {
-                    var d = new Date(item.logdate);
-                    if ((d.getYear() == d2.getYear()) && (d.getMonth() == d2.getMonth())) {
-                        logs.push(item);
-                    }
-                });
-                angular.forEach($scope.profile.terapies, function (item) {
-                    var d = new Date(item.logdate);
-                    if ((d.getYear() == d2.getYear()) && (d.getMonth() == d2.getMonth())) {
-                        if (item.isslow) {
-                            slowvalues.push(item);
-                        } else {
-                            fastvalues.push(item);
-                        }
-                    }
-                });
-                $scope.selectedreport.description = moment($scope.date).format('MMMM YYYY');
+        });
+        return logs;
+    }
+    
+    function getLogsByMonth() {
+        var logs = [];
+        var d2 = new Date($scope.date);
+        angular.forEach($scope.profile.logs, function (item) {
+            var d = new Date(item.logdate);
+            if ((d.getYear() == d2.getYear()) && (d.getMonth() == d2.getMonth())) {
+                logs.push(item);
             }
-            if ($scope.selectedreport.name == "Week") {
-                var weekdates = getWeekDays($scope.date);
-                angular.forEach($scope.profile.logs, function (item) {
-                    angular.forEach(weekdates, function (weekDay) {
-                        var a1 = moment(item.logdate).startOf('day').toDate();
-                        var a2 = weekDay.startOf('day').toDate();
-                        if (+a1 === +a2) {
-                            logs.push(item);
-                        }
-                    });
-                });
-                angular.forEach($scope.profile.terapies, function (item) {
-                    angular.forEach(weekdates, function (weekDay) {
-                        var a1 = moment(item.logdate).startOf('day').toDate();
-                        var a2 = weekDay.startOf('day').toDate();
-                        if (+a1 === +a2) {
-                            if (item.isslow) {
-                                slowvalues.push(item);
-                            } else {
-                                fastvalues.push(item);
-                            }
-                        }
-                    });
-                });
-                $scope.selectedreport.description = "from " + moment($scope.date).day("Sunday").format('DD MM YYYY') + " to " + moment($scope.date).day("Saturday").format('DD MM YYYY');
-            }
-            if ($scope.selectedreport.name == "Day") {
-                angular.forEach($scope.profile.logs, function (item) {
-                    var a1 = moment(item.logdate).startOf('day').toDate();
-                    var a2 = moment($scope.date).startOf('day').toDate();
-                    if (+a1 === +a2) {
-                        logs.push(item);
-                    }
-                });
-                angular.forEach($scope.profile.terapies, function (item) {
-                    var a1 = moment(item.logdate).startOf('day').toDate();
-                    var a2 = moment($scope.date).startOf('day').toDate();
-                    if (+a1 === +a2) {
-                        if (item.isslow) {
-                            slowvalues.push(item);
-                        } else {
-                            fastvalues.push(item);
-                        }
-                    }
-                });
-                $scope.selectedreport.description = moment($scope.date).format('DD MM YYYY');
-            }
-            $scope.graph.setData(logs);
-            
-            // Fill terapies graph data starting with fast values
-            angular.forEach(fastvalues, function (item) {
-                terapies.push({ 'logdate': item.logdate, 'fastvalues': item.terapyvalue, 'slowvalues': 0 });
+        });
+        return logs;
+    }
+    
+    function getLogsByWeeks() {
+        var logs = [];
+        var weekdates = getWeekDays($scope.date);
+        angular.forEach($scope.profile.logs, function (item) {
+            angular.forEach(weekdates, function (weekDay) {
+                var a1 = moment(item.logdate).startOf('day').toDate();
+                var a2 = weekDay.startOf('day').toDate();
+                if (+a1 === +a2) {
+                    logs.push(item);
+                }
             });
-            // Fill slow values
-            angular.forEach(slowvalues, function (item) {
-                insertOrUpdateSlowValue(terapies, item);
-            });
-            function insertOrUpdateSlowValue(terapyList, item) {
-                var found = false;
-                angular.forEach(terapyList, function (terapy) {
-                    if (terapy.logdate == item.logdate) {
-                        terapy.slowvalues = item.terapyvalue;
-                        found = true;
-                    }
-                });
-                if (found == false) {
-                    terapies.push({ 'logdate': item.logdate, 'fastvalues': 0, 'slowvalues': item.terapyvalue });
+        });
+        return logs;
+    }
+    
+    function getLogsByDay() {
+        var logs = [];
+        angular.forEach($scope.profile.logs, function (item) {
+            var a1 = moment(item.logdate).startOf('day').toDate();
+            var a2 = moment($scope.date).startOf('day').toDate();
+            if (+a1 === +a2) {
+                logs.push(item);
+            }
+        });
+        return logs;
+    }
+    
+    function getTerapiesByYear() {
+        var fastvalues = [];
+        var slowvalues = [];
+        var d2 = new Date($scope.date);
+        angular.forEach($scope.profile.terapies, function (item) {
+            var d = new Date(item.logdate);
+            if (d.getYear() == d2.getYear()) {
+                if (item.isslow) {
+                    slowvalues.push(item);
+                } else {
+                    fastvalues.push(item);
                 }
             }
+        });
+        return { 'fastvalues': fastvalues, 'slowvalues': slowvalues };
+    }
 
-            $scope.terapiesGraph.setData(terapies);
-            $scope.logs = logs;
-            $scope.terapies = terapies;
-            $scope.selectedreport.average = getAverage();
+    function getTerapiesByMonth() {
+        var fastvalues = [];
+        var slowvalues = [];
+        var d2 = new Date($scope.date);
+        angular.forEach($scope.profile.terapies, function (item) {
+            var d = new Date(item.logdate);
+            if ((d.getYear() == d2.getYear()) && (d.getMonth() == d2.getMonth())) {
+                if (item.isslow) {
+                    slowvalues.push(item);
+                } else {
+                    fastvalues.push(item);
+                }
+            }
+        });
+        return { 'fastvalues': fastvalues, 'slowvalues': slowvalues };
+    }
+
+    function getTerapiesByWeeks() {
+        var fastvalues = [];
+        var slowvalues = [];
+        var weekdates = getWeekDays($scope.date);
+        angular.forEach($scope.profile.terapies, function (item) {
+            angular.forEach(weekdates, function (weekDay) {
+                var a1 = moment(item.logdate).startOf('day').toDate();
+                var a2 = weekDay.startOf('day').toDate();
+                if (+a1 === +a2) {
+                    if (item.isslow) {
+                        slowvalues.push(item);
+                    } else {
+                        fastvalues.push(item);
+                    }
+                }
+            });
+        });
+        return { 'fastvalues': fastvalues, 'slowvalues': slowvalues };
+    }
+
+    function getTerapiesByDay() {
+        var fastvalues = [];
+        var slowvalues = [];
+        angular.forEach($scope.profile.terapies, function (item) {
+            var a1 = moment(item.logdate).startOf('day').toDate();
+            var a2 = moment($scope.date).startOf('day').toDate();
+            if (+a1 === +a2) {
+                if (item.isslow) {
+                    slowvalues.push(item);
+                } else {
+                    fastvalues.push(item);
+                }
+            }
+        });
+        return { 'fastvalues': fastvalues, 'slowvalues': slowvalues };
+    }
+    
+    function getLogs(reportname) {
+        if (reportname == "Year") {
+            return getLogsByYear();
         }
+        if (reportname == "Month") {
+            return getLogsByMonth();
+        }
+        if (reportname == "Week") {
+            return getLogsByWeeks();
+        }
+        if (reportname == "Day") {
+            return getLogsByDay();
+        }
+        return null;
+    }
+    
+    function getTerapies(reportname) {
+        var terapies = [];
+        var terapyObject = {};
+        if (reportname == "Year") {
+            terapyObject = getTerapiesByYear();
+        }
+        if (reportname == "Month") {
+            terapyObject = getTerapiesByMonth();
+        }
+        if (reportname == "Week") {
+            terapyObject = getTerapiesByWeeks();
+        }
+        if (reportname == "Day") {
+            terapyObject = getTerapiesByDay();
+        }
+        // Fill terapies graph data starting with fast values
+        angular.forEach(terapyObject.fastvalues, function (item) {
+            terapies.push({ 'logdate': item.logdate, 'fastvalues': item.terapyvalue, 'slowvalues': 0 });
+        });
+        // Fill slow values
+        angular.forEach(terapyObject.slowvalues, function (item) {
+            insertOrUpdateSlowValue(terapies, item);
+        });
+        function insertOrUpdateSlowValue(terapyList, item) {
+            var found = false;
+            angular.forEach(terapyList, function (terapy) {
+                if (terapy.logdate == item.logdate) {
+                    terapy.slowvalues = item.terapyvalue;
+                    found = true;
+                }
+            });
+            if (found == false) {
+                terapies.push({ 'logdate': item.logdate, 'fastvalues': 0, 'slowvalues': item.terapyvalue });
+            }
+        }
+
+        return terapies;
+    }
+    
+    function getReportDescription(reportname) {
+        if (reportname == "Year") {
+            return moment($scope.date).format('YYYY');
+        }
+        if (reportname == "Month") {
+            return moment($scope.date).format('MMMM YYYY');
+        }
+        if (reportname == "Week") {
+            return "from " + moment($scope.date).day("Sunday").format('DD MM YYYY') + " to " + moment($scope.date).day("Saturday").format('DD MM YYYY');
+        }
+        if (reportname == "Day") {
+            return moment($scope.date).format('DD MM YYYY');
+        }
+        return null;
     }
 }]);
