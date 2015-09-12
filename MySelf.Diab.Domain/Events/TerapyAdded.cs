b1e72f9741c012e056cb7044fbf6ab45c0ff95a7
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2015-09-12 18:05:35 +0100

    fixed previous commit

--- MySelf.Diab.Domain/Events/TerapyAdded.cs
+++ MySelf.Diab.Domain/Events/TerapyAdded.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace MySelf.Diab.Domain.Events
 {
-    public class GlucoseLevelAdded
+    public class TerapyAdded
     {
         public string Message { get; private set; }
-
         public int Value { get; private set; }
-
+        public bool IsSlow { get; private set; }
         public DateTime LogDate { get; private set; }
 
-        public GlucoseLevelAdded(string message, int value, DateTime logDate)
+        public TerapyAdded(string message, int value, bool isSlow, DateTime logDate)
         {
             Message = message;
             Value = value;
+            IsSlow = isSlow;
             LogDate = logDate;
         }
     }
