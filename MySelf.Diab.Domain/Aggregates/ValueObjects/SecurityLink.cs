6c2d50a8d209425ffd058b52a35d7bab2a386279
riccardone <riccardo@dinuzzo.it>
2015-12-02 09:45:35 +0000

    removed eventstore; update webmatrix.webdata using a nuget (legacy)

--- MySelf.Diab.Domain/Aggregates/ValueObjects/SecurityLink.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MySelf.Diab.Domain.Aggregates.ValueObjects
-{
-    public class SecurityLink
-    {
-        public int Id { get; set; }
-        public string Link { get; set; }
-    }
-}
--- MySelf.Diab.Tests/LogProfileTests.cs
+++ MySelf.Diab.Tests/LogProfileTests.cs
@@ -11,15 +11,15 @@ namespace MySelf.Diab.Tests
         [TestMethod]
         public void CreateLogProfile()
         {
-            var repo = new EventStoreDomainRepository();
-            var logCommands = new EventStoreLogCommands(repo);
-            var message = new Model.LogProfile {
-                GlobalId = new Guid("6b4b020d-12c6-42ca-8100-d3c6d6bf5f36"),
-                Name= "Default",
-                Person = new Model.Person { Id = 2, Email = "riccardo@dinuzzo.it" }
-            };
+            //var repo = new EventStoreDomainRepository();
+            //var logCommands = new EventStoreLogCommands(repo);
+            //var message = new Model.LogProfile {
+            //    GlobalId = new Guid("6b4b020d-12c6-42ca-8100-d3c6d6bf5f36"),
+            //    Name= "Default",
+            //    Person = new Model.Person { Id = 2, Email = "riccardo@dinuzzo.it" }
+            //};
 
-            logCommands.AddLogProfile(message);
+            //logCommands.AddLogProfile(message);
         }
     }
 }
