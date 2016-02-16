e00d8e24fb91165c43ba5a25c2d6dcdca0fb645e
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2016-02-16 18:42:33 +0000

    cleaned commented code

--- MySelf.WebClient/App_Start/BundleConfig.cs
+++ MySelf.WebClient/App_Start/BundleConfig.cs
@@ -10,33 +10,6 @@ namespace MySelf.WebClient
         {
             bundles.UseCdn = false;   //enable CDN support
 
-            //bundles.Add(new ScriptBundle("~/bundles/raphael",
-            //    "//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js").Include(
-            //    "~/Scripts/raphael-min.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqplot")
-            //    .Include("~/Scripts/jquery.jqplot.min.js")
-            //    .Include("~/Scripts/jplotplugins/jqplot.highlighter.min.js")
-            //    .Include("~/Scripts/jplotplugins/jqplot.cursor.min.js")
-            //    .Include("~/Scripts/jplotplugins/jqplot.dateAxisRenderer.min.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jquery")
-            //    .Include("~/Scripts/jquery-2.0.3.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jslibs")
-            //                .Include("~/Scripts/toastr.min.js")
-            //                .Include("~/Scripts/moment.min.js")
-            //    );
-
-            //bundles.Add(new ScriptBundle("~/bundles/prologue")
-            //                .Include("~/Scripts/skel.min.js")
-            //                .Include("~/Scripts/skel-panels.min.js")
-            //                .Include("~/Scripts/init.js")
-            //    );
-
-            //bundles.Add(new ScriptBundle("~/bundles/ajaxlogin").Include(
-            //    "~/app/js/ajaxlogin.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/superciccio")
                             .Include("~/Scripts/toastr.js")
                             .Include("~/Scripts/moment.js")
@@ -54,34 +27,11 @@ namespace MySelf.WebClient
                             .Include("~/Scripts/angular-ui/ui-bootstrap.js")
                             );
 
-            //bundles.Add(new ScriptBundle("~/bundles/angular")
-            //                .Include("~/Scripts/angular.js")
-            //                .Include("~/Scripts/angular-resource.js")
-            //                .Include("~/Scripts/angular-route.js")
-            //                .Include("~/Scripts/angular-animate.js")
-            //                .Include("~/Scripts/angular-ui/ui-bootstrap.js")
-            //                //.Include("~/Scripts/ui-bootstrap-0.6.0.min.js")
-            //                //.Include("~/Scripts/ui-bootstrap-tpls-0.6.0.min.js")
-            //                //.Include("~/Scripts/ng-table.js")
-            //                );
-
             bundles.Add(new ScriptBundle("~/bundles/myselflog")
                 .Include("~/app/js/*.js")
                 .Include("~/app/js/services/*.js")
                 .Include("~/app/js/controllers/*.js")
                 );
-
-            //bundles.Add(new ScriptBundle("~/bundles/jquerymobile")
-            //    .Include("~/Scripts/jquery.mobile*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui")
-            //    .Include("~/Scripts/jquery-ui-{version}.js")
-            //    //.Include("~/Scripts/dateTimePicker.js")
-            //    );
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.unobtrusive*",
-            //            "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
